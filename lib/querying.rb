87f7091b99ef2c71559fecf4ac414d9dadb5e946
Joe Ganobsik <jganobsik@gmail.com>
2019-01-05 19:54:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors, subgenres
-  FULL JOIN authors.id = series.author_id, subgenres.id = series.subgenre_id "
+  INNER JOIN authors.id = series.author_id, subgenres.id = series.subgenre_id 
+  GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
