f1be7d5c0627c617bc159b848d8793e5ace1d05d
Joe Ganobsik <jganobsik@gmail.com>
2019-01-05 20:02:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON characters.series_id = series.id
   ORDER BY count(species)
-  WHERE species = 'human'"
+  WHERE characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
