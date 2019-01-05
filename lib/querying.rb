59b3a3cc455d1472bd25261dd87e48a538ee639d
Joe Ganobsik <jganobsik@gmail.com>
2019-01-05 20:04:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
   ON characters.series_id = series.id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(characters.species)
+  ORDER BY COUNT(characters.species) DESC LIMIT 1
  "
 end
 
