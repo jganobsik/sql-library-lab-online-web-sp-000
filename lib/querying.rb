94262ab4b80a2caba0f3aa52d8f92a4e8866b7d4
Joe Ganobsik <jganobsik@gmail.com>
2019-01-05 20:00:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title
   FROM series
-  INNER JOIN "
+  INNER JOIN characters
+  ON series"
 end
 
 def select_character_names_and_number_of_books_they_are_in
