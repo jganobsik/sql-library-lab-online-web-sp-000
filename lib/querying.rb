323dfb75c5b2c594a9da46904a4736f8f4255d6a
Joe Ganobsik <jganobsik@gmail.com>
2019-01-05 19:46:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year 
   FROM books 
-  INNER JOIN series "
+  INNER JOIN series ON "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
